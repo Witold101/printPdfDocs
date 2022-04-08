3f37000f0fbba1d1398b520652d2e0df1917327d
Witold101 <vi_stal@hotmail.com>
2022-04-08 20:32:12 +0200

    Add Slownie zlotych

--- src/main/java/lt/vistar/RunApp.java
+++ src/main/java/lt/vistar/RunApp.java
@@ -32,21 +32,21 @@ public class RunApp {
 
     public static void main(String[] args) throws IOException, DocumentException {
         ListTrasa listTrasa = new ListTrasa();
-        listTrasa.buildList(new Trasa("Bydgoszcz",new GregorianCalendar(2022,01,01,
-                14,30),"Wiskitki",new GregorianCalendar(2022,01,01,
+        listTrasa.buildList(new Trasa("Bydgoszcz",new GregorianCalendar(2022, Calendar.FEBRUARY,01,
+                14,30),"Wiskitki",new GregorianCalendar(2022, Calendar.FEBRUARY,01,
                 17,30),235));
-        listTrasa.buildList(new Trasa("Wiskitki",new GregorianCalendar(2022,01,02,
-                8,30),"Łuków",new GregorianCalendar(2022,01,02,
+        listTrasa.buildList(new Trasa("Wiskitki",new GregorianCalendar(2022, Calendar.FEBRUARY,02,
+                8,30),"Łuków",new GregorianCalendar(2022, Calendar.FEBRUARY,02,
                 10,40),170));
-        listTrasa.buildList(new Trasa("Łuków",new GregorianCalendar(2022,01,02,
-                12,00),"Bydgoszcz",new GregorianCalendar(2022,01,02,
+        listTrasa.buildList(new Trasa("Łuków",new GregorianCalendar(2022, Calendar.FEBRUARY,02,
+                12,00),"Bydgoszcz",new GregorianCalendar(2022, Calendar.FEBRUARY,02,
                 16,50),410));
         Customer customer = getCustomer("PPO PP", "Dworcowa 25,47-100 Strzelce Opolskie");
         PolecenieWyjazdu polecenieWyjazdu = getPolecenieWyjazdu("03",
-                        new GregorianCalendar(2022,2,20),
+                        new GregorianCalendar(2022, Calendar.MARCH,20),
                         "Vitalij", "Vasylius","Dyrektor handlowy", customer,
-                        new GregorianCalendar(2022,2,20),
-                        new GregorianCalendar(2022,2,21),
+                        new GregorianCalendar(2022, Calendar.MARCH,20),
+                        new GregorianCalendar(2022, Calendar.MARCH,21),
                         "Samochód osobowy prywatny", new BigDecimal(142.04)
                                                                 .setScale(2, RoundingMode.HALF_UP),
                     listTrasa);
